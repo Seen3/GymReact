9aba97f4ef226d1cfce89d1990173dc84aff05fa
Seen3 <littlefinger331@gmail.com>
2022-05-11 15:44:07 +0530

    login frontend done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+function Login() {
+  return (
+    <div>
+        <h2>Welcome to the Login Page</h2> 
+      <form className='loginform'>
+      <label for="uname"><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="uname" required/>
+      <label for="pass"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="pass" required/>
+      <button type="submit">Login</button>
+      <label>
+      <input type="checkbox" name="remember"/>Remember Me
+      </label>
+      </form>
+      <button className='knowMore'>
+        KNOW MORE
+      </button>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+        <p>
+          That feels like an existential question, don't you
+          think?
+        </p>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </>
+  );
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
   );
 }
