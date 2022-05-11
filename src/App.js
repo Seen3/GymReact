13ca90a16b1d0c4c0c0621744941905f2aeac6db
Seen3 <littlefinger331@gmail.com>
2022-05-11 17:52:19 +0530

    added a homepage

--- src/App.js
+++ src/App.js
@@ -3,10 +3,34 @@ import { Routes, Route} from "react-router-dom";
 import { Login } from './pages/login';
 import {About} from './pages/about';
 
+function Home(){
+
+  return(
+    <div>
+      <table>
+        <tr>
+        <th>Mon</th>
+        <th>Tue</th>
+        <th>Wed</th>
+        <th>Thu</th>
+        <th>Fri</th>
+        <th>Sat</th>
+        <th>Sun</th>
+        </tr>
+        <tr>
+        </tr>
+      </table>
+    </div>
+    
+  )
+}
+
+
 function App() {
   return (
     <div className="App">
         <Routes>
+        <Route path='/' element={<Home/>} />
         <Route path="/Login" element={<Login />} />
         <Route path="/About" element={<About />} />
       </Routes>
