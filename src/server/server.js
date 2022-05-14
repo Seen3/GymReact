d394b276205cc581c7e6c96ce199d6ac59f63480
Seen3 <littlefinger331@gmail.com>
2022-05-14 20:04:54 +0530

    Express Server

--- src/server/server.js
+++ src/server/server.js
@@ -1,4 +1,4 @@
-const url="mongodb+srv://Seen:APqxtOl2m9eakm8T@gymdata.4cw5q.mongodb.net/LoginData.Uspw?retryWrites=true&w=majority"
+/*const url="mongodb+srv://Seen:APqxtOl2m9eakm8T@gymdata.4cw5q.mongodb.net/LoginData.Uspw?retryWrites=true&w=majority"
 let mongoose=require('mongoose')
 mongoose.connect(url,{newUrlParser:true,useUnifiedTopology:true});
 let db=mongoose.connection;
@@ -11,3 +11,24 @@ let userPass=new Schema(
         pass:{type:String,required:true,maxLength:100},
     }
 )
+*/
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config({ path: "./config.env" });
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/record"));
+// get driver connection
+const dbo = require("./db/conn");
+ 
+app.listen(port, () => {
+  // perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+ 
+  });
+  console.log(`Server is running on port: ${port}`);
+});
